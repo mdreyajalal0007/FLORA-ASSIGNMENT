00a1de2773646a54567f783e623c56a1341a3c2e
Reyaj Alam <mdreyajalal0007@gmail.com>
2021-06-05 23:07:11 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fauna';
+  myimage:string ="assets/images/tiger.jpg";
+  myimag: string ="assets/images/heal.png"
+
+
+  onClick(){
+    alert("You can find all animals from here")
+  }
+  onc(){
+    alert("Corona virus updates")
+  }
+
+  onclk(){
+    alert("All Mummals is here")
+  }
+
+a1(){
+  alert("You are Looking About Single Animal Page")
+}
+a2(){
+  alert("You are Looking About Groups Animal Pages")
+}
+a3(){
+  alert("Hey Raja here")
+}
+
 }
